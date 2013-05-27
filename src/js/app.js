62e7cc1322379449243e4504aa5fdf16f712daff
unknown <flo@GUMMIBAUM.hq.focusconsulting.ch>
2013-05-27 15:37:59 +0200

    skjdf

--- src/js/app.js
+++ src/js/app.js
@@ -15,43 +15,51 @@ function App() {
 	
 	this.integrator = new RungeKuttaIntegrator();
 	
-	this.paintBorder = function (ctx, cx, cy, H, r) {
+	this.paintFunnel = function (ctx, cx, cy, H, r, draw) {
 		ctx.beginPath();
 		
 		var rh1 = r(H);
 		var rh0 = r(0);
 		
-		ctx.moveTo(cx - rh1, cy - H / 2);
-		ctx.lineTo(cx + rh1, cy - H / 2);
+		ctx.moveTo(cx - rh1, cy - H);
+		ctx.lineTo(cx + rh1, cy - H);
 		
 		for (var h = H - 1; h > 0; --h) {
 			var rh = r(h);
-			ctx.lineTo(cx + rh, cy + H / 2 - h);
+			ctx.lineTo(cx + rh, cy - h);
 		}
 		
-		ctx.lineTo(cx + rh0, cy + H / 2);
-		ctx.lineTo(cx - rh0, cy + H / 2);
+		ctx.lineTo(cx + rh0, cy);
+		ctx.lineTo(cx - rh0, cy);
 
 		for (var h = 1; h < H; h++) {
 			var rh = r(h);
-			ctx.lineTo(cx - rh, cy + H / 2 - h);
+			ctx.lineTo(cx - rh, cy - h);
 		}
 
-		ctx.lineTo(cx - rh1, cy - H / 2);
+		ctx.lineTo(cx - rh1, cy - H);
 
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = "white";
-		ctx.stroke();		
+		draw.call(ctx);
+		
 	}
-
+	
 	this.paint = function () {
 		
 		var cx = self.canvasWidth / 2;
-		var cy = self.canvasHeight / 2;
-				
-		self.paintBorder(self.context, cx, cy, self.funnel.height(), self.funnel.radius);
-		
+		var cy = self.canvasHeight / 2 + self.funnel.height() / 2;
+			
+		var fillHeight = self.funnel.height() / 2;
 		//var fillHeight = self.integrator.integrate(0, );
+
+		self.context.lineWidth = 0;
+		self.context.strokeStyle = "none";
+		self.context.fillStyle = "blue";
+		self.paintFunnel(self.context, cx, cy, fillHeight, self.funnel.radius, self.context.fill);
+
+		self.context.lineWidth = 2;
+		self.context.strokeStyle = "white";
+		self.paintFunnel(self.context, cx, cy, self.funnel.height(), self.funnel.radius, self.context.stroke);
+		
 		
 		//console.log(fillHeight);
 		
