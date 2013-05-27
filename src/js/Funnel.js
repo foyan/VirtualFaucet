d55e48bf24d57003bf78cd43df9e42b1a590fad7
unknown <flo@GUMMIBAUM.hq.focusconsulting.ch>
2013-05-27 09:46:09 +0200

    ksjdf

--- src/js/Funnel.js
+++ src/js/Funnel.js
@@ -2,14 +2,18 @@ function Funnel() {
 	
 	var self = this;
 	
-	this.topPerimeter = ko.observable(200);
+	this.topPerimeter = ko.observable(150);
 	
 	this.bottomPerimeter = ko.observable(10);
 	
-	this.height = ko.observable(50);
+	this.height = ko.observable(200);
 	
-	this.border = function (h, tp, bp) {
-		return self.bottomPerimeter + h / self.height * (self.topPerimeter - self.bottomPerimeter);
+	this.volume = function (h, H, A1, A0) {
+		return A0 + h / H * (A1 - A0);
+	}
+	
+	this.getVolume = function (h) {
+		return self.volume(h, self.height(), self.topPerimeter(), self.bottomPerimeter());
 	}
 	
 }
--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,8 @@ function App() {
 	
 	this.canvas = $("#vf").get(0);
 	
-	this.canvasWidth = 500;
-	this.canvasHeight = 500;
+	this.canvasWidth = self.canvas.width;
+	this.canvasHeight = self.canvas.height;
 	
 	this.context = self.canvas.getContext("2d");
 	
@@ -18,9 +18,32 @@ function App() {
 		
 		console.log("center: (" + centerX + ", " + centerY + ")");
 		
+		self.context.beginPath();
 		
+		self.context.moveTo(centerX - self.funnel.topPerimeter() / 2, centerY - self.funnel.height() / 2);
+		self.context.lineTo(centerX + self.funnel.topPerimeter() / 2, centerY - self.funnel.height() / 2);
 		
-	}
+		for (var h = self.funnel.height() - 1; h > 0; --h) {
+			var peri = self.funnel.getVolume(h);
+			self.context.lineTo(centerX + peri / 2, centerY + self.funnel.height() / 2 - h);
+		}
+		
+		self.context.lineTo(centerX + self.funnel.bottomPerimeter() / 2, centerY + self.funnel.height() / 2);
+		self.context.lineTo(centerX - self.funnel.bottomPerimeter() / 2, centerY + self.funnel.height() / 2);
+
+		for (var h = 1; h < self.funnel.height(); h++) {
+			var peri = self.funnel.getVolume(h);
+			self.context.lineTo(centerX - peri / 2, centerY + self.funnel.height() / 2 - h);
+		}
+
+		self.context.lineTo(centerX - self.funnel.topPerimeter() / 2, centerY - self.funnel.height() / 2);
+
+		self.context.lineWidth = 1;
+		self.context.strokeStyle = "white";
+		
+		self.context.stroke();
+				
+	};
 	
 }
 
