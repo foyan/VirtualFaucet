67cc69c7ce037f4670fcab7199e119fa119df7bb
unknown <flo@GUMMIBAUM.hq.focusconsulting.ch>
2013-05-28 13:35:59 +0200

    sjdf

--- src/js/app.js
+++ src/js/app.js
@@ -179,6 +179,13 @@ $(function () {
 	ko.applyBindings(app);
 	
 	app.start();
+	
+// Creates canvas 640 × 480 at 10, 50
+var r = Raphael("graph");
+// Creates pie chart at with center at 320, 200,
+// radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
+r.linechart(320, 240, 100, [55, 20, 13, 32, 5, 1, 2]);
+
 })
 
 
